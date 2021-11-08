88502ec95cce79297c210256e49cf93998b5b7b5
kimoland <85030146+kimoland@users.noreply.github.com>
2021-11-08 10:21:33 +0330

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -53,7 +53,7 @@ async def download(event):
             id_hex = hex(msg.id)[2:]
             id = f"{id_hex}/{get_file_name(msg)}"
             bot_url = f"t.me/{username_bot}?start={id_hex}"
-            await event.reply(f"✅فایل شما با موفقیت به لینک تبدیل شد\n🌐 Link : {Config.DOMAIN}/{id}\n🆔 @{Config.CHANNEL_USERNAME}")
+            await event.reply(f"✅فایل شما با موفقیت به لینک تبدیل شد\n🌐 Link : {Config.DOMAIN}/{id}\n\n🆔 @{Config.CHANNEL_USERNAME}")
             return
 
         elif id_msg := re.search("/start (.*)", event.raw_text ):
