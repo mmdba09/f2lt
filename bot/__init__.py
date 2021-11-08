d9fd1d33bab3757e0b932635a4a570521a2ff467
kimoland <85030146+kimoland@users.noreply.github.com>
2021-11-08 09:45:14 +0330

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -38,7 +38,7 @@ async def download(event):
                     participant = event.sender_id
                     ))
             except errors.UserNotParticipantError:
-                await event.reply(f"🌀برای استفاده از ربات ابتدا باید در کانال ما عضو بشی\n💠برای عضویت روی ایدی زیر کلیک کنید\n🔸\n@{Config.CHANNEL_USERNAME}🔹پس از عضویت دستور /start را ارسال کنید")
+                await event.reply(f"First join to our official channel to access the bot or get the newest news about the bot\n\n@{Config.CHANNEL_USERNAME}\n\nAfter that /start the bot aging.")
                 return
         
         if event.file :
@@ -53,7 +53,7 @@ async def download(event):
             id_hex = hex(msg.id)[2:]
             id = f"{id_hex}/{get_file_name(msg)}"
             bot_url = f"t.me/{username_bot}?start={id_hex}"
-            await event.reply(f"✅فایل شما با موفقیت به لینک تبدیل شد\n🌐 Link : \n{Config.DOMAIN}🆔 @{Config.CHANNEL_USERNAME}")
+            await event.reply(f"Link to download file: \n\n📎 : {Config.DOMAIN}/{id}\n\n🤖 : {bot_url}")
             return
 
         elif id_msg := re.search("/start (.*)", event.raw_text ):
@@ -82,7 +82,7 @@ async def download(event):
                         await forward_reply.edit(f"📎 : {Config.DOMAIN}/{id_name}\n\n🤖 : {bot_url}",link_preview=True)
                 return
         if pv:
-            await event.reply(f"🌀خوش آمدید\n🔰برای استفاده از ربات کافی است\nفایل خود را ارسال کرده و سپس لینک آن را دریافت کنید\n\n🆔 @{Config.CHANNEL_USERNAME}")
+            await event.reply("Send an file to get a link to download it")
         
 
     elif event.is_channel:
